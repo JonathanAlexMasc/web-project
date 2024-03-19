faee3605fc89a3e2d6037d6609e1f77d8589dc13
Jonathan Mascarenhas <mascarenhasjonathan@gmail.com>
2024-03-18 19:17:20 -0600

    have html rendering properly

--- src/index.js
+++ src/index.js
@@ -1,9 +1,29 @@
 let http = require('http');
+let fs = require('fs');
 
 let server = http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.write('Hello World!')
+
+    let host = "http://" + req.headers.host;
+    const query = new URL(req.url, host); //optional second paramter, that should be the root of the website
+    let path = "." + query.pathname;
+
+    if( path ==="./" ) //index page
+        path = "./index.html"
+
+    let extension = path.split('.').pop();
+    if(extension === "html"){
+        try{
+            let content = fs.readFileSync(path);
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.write(content);
+
+        }catch(err){
+            //data dump
+            console.log(err);
+        }
+    }
+
     res.end();
-})
+});
 
 server.listen(8080);
