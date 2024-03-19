332b1d9dda139a4fd41d445f4c2daf1f8865dc94
Jonathan Mascarenhas <mascarenhasjonathan@gmail.com>
2024-03-18 19:18:09 -0600

    css also renders properly

--- src/index.js
+++ src/index.js
@@ -1,29 +1,50 @@
 let http = require('http');
 let fs = require('fs');
+let pathModule = require('path');
 
 let server = http.createServer(function (req, res) {
-
     let host = "http://" + req.headers.host;
-    const query = new URL(req.url, host); //optional second paramter, that should be the root of the website
+    const query = new URL(req.url, host);
     let path = "." + query.pathname;
 
-    if( path ==="./" ) //index page
-        path = "./index.html"
-
-    let extension = path.split('.').pop();
-    if(extension === "html"){
-        try{
-            let content = fs.readFileSync(path);
-            res.writeHead(200, {'Content-Type': 'text/html'});
-            res.write(content);
-
-        }catch(err){
-            //data dump
-            console.log(err);
-        }
+    if (path === "./") {
+        path = "./index.html";
     }
 
-    res.end();
+    let extension = pathModule.extname(path).toLowerCase();
+
+    try {
+        let content;
+        let contentType;
+
+        // Serve HTML files
+        if (extension === ".html") {
+            content = fs.readFileSync(path);
+            contentType = 'text/html';
+        }
+        // Serve CSS files
+        else if (extension === ".css") {
+            content = fs.readFileSync(path);
+            contentType = 'text/css';
+        }
+        // Add other file types as needed
+
+        // Send response
+        if (content) {
+            res.writeHead(200, {'Content-Type': contentType});
+            res.write(content);
+            res.end();
+        } else {
+            // Return 404 if file type is not supported
+            res.writeHead(404);
+            res.end();
+        }
+    } catch(err) {
+        // Log any errors
+        console.log(err);
+        res.writeHead(500);
+        res.end();
+    }
 });
 
 server.listen(8080);
